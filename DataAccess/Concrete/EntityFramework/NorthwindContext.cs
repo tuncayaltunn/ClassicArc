e5fe540396181c95a813dc9f10301b72e052d6d4
taft <altun.tuncay@outlook.com>
2024-12-08 11:12:04 +0300

    Northwind Connection

--- DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -11,11 +11,8 @@ namespace DataAccess.Concrete.EntityFramework
 		//}
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {//User ID=root;Password=myPassword;
-
-            //optionsBuilder.UseNpgsql("User ID=postgres;Password=123456;Host=localhost;Port=5432;Database=Northwind;Pooling=true;min_pool_size=0;max_pool_size=1024;connection_lifetime=0;trusted_connection=True;TrustServerCertificate=True");
+        {
             optionsBuilder.UseNpgsql("User ID=postgres;Password=123456;Host=localhost;Port=5432;Database=Northwind;");
-            //base.OnConfiguring(optionsBuilder);
         }
 
         public DbSet<Category> categories { get; set; }

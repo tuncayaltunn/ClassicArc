cfd99627a4f01300a463f6456252b16420387f32
taft <altun.tuncay@outlook.com>
2024-12-10 22:38:17 +0300

    Autofac

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,14 +1,31 @@
+using Autofac;
+using System.Reflection.Metadata;
 using Business.Abstract;
 using Business.Concrete;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
+using Business.DependencyResolvers.Autofac;
+using Microsoft.AspNetCore.Hosting;
+using Autofac.Extensions.DependencyInjection;
+using WebAPI;
+using Microsoft.Extensions.Configuration;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddSingleton<IProductService,ProductManager>();
-builder.Services.AddSingleton<IProductDal,EfProductDal>();
+//builder.Services.AddSingleton<IProductService,ProductManager>();
+//builder.Services.AddSingleton<IProductDal,EfProductDal>();
+
+
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
+// Register services directly with Autofac here.
+// Don't call builder.Populate(), that happens in AutofacServiceProviderFactory.
+builder.Host.ConfigureContainer<ContainerBuilder>(
+   builder => builder.RegisterModule(new AutofacBusinessModule()));
+
 
 builder.Services.AddControllers();
 
